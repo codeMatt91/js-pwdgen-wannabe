3e5123cf10b0103f676000b8e7f59b0c3b156d1f
codeMatt91 <matteo.imbimbo@yahoo.it>
2021-12-20 17:05:59 +0100

    Created const name , surname in JS

--- script.js
+++ script.js
@@ -8,5 +8,17 @@ poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21
 
+1- chiedere nome all'utente
+2-chiedere il cognome
+3-chiedere il colore preferito
+4-recuperare dal DOM dove stampare
+5-stampare password
 */
 
+// punto 1 
+const userName = prompt("Qual'è il tuo nome?");
+console.log (userName);
+
+// punto 2
+const userSurname = prompt("Qual'è il tuo cognome")
+console.log (userSurname);
